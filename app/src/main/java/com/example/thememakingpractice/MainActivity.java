dbe2ccab3ccb9bce151ad4a6baba10c566e2dd87
JensLundsgaard <111328305+JensLundsgaard@users.noreply.github.com>
2022-09-09 12:07:08 -0500

    Added some functionality

--- app/src/main/java/com/example/thememakingpractice/MainActivity.java
+++ app/src/main/java/com/example/thememakingpractice/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.thememakingpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void setTheme1(View v){
+        setTheme(R.style.Theme_MyTheme);
+        setContentView(R.layout.activity_main);
+    }
+    public void setTheme2(View v){
+        setTheme(R.style.Theme_MyTheme2);
         setContentView(R.layout.activity_main);
     }
 }
