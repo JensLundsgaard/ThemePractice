492c68cfe33a6c9b3e218088e8936870b44b5091
JensLundsgaard <111328305+JensLundsgaard@users.noreply.github.com>
2022-09-16 12:53:36 -0500

    Added more functionality, and a gridLayout to the second screeen

--- app/src/main/java/com/example/thememakingpractice/MainActivity.java
+++ app/src/main/java/com/example/thememakingpractice/MainActivity.java
@@ -5,28 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     boolean isTheme1;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 
     public void onClick(View v){
-        switch (v.getId()){
-            case R.id.button:
-                isTheme1 = ((ToggleButton)findViewById(R.id.toggleButton)).isChecked();
-                // Create the intent, envelope and call its instructor and tell it where its intent is coming from 1st parameter
-                // and where it is going 2nd parameter.
-                Intent intent = new Intent(this, SecondActivity.class);
-                // put data inside intent to send to the receiver (optional)
-                intent.putExtra("THEME", isTheme1);
-                // Start activity, switch screen, mail envelope
-                startActivity(intent);
-            break;
-        }
+        editText = findViewById(R.id.editText1);
+        String text = editText.getText().toString();
+        isTheme1 = ((ToggleButton)findViewById(R.id.toggleButton)).isChecked();
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("THEME", isTheme1);
+        intent.putExtra("TEXT", text);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/thememakingpractice/SecondActivity.java
+++ app/src/main/java/com/example/thememakingpractice/SecondActivity.java
@@ -4,20 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     boolean isTheme1;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
         Intent intent = getIntent();
-        // extract the String data that we labeled as username
-        isTheme1 = intent.getBooleanExtra("THEME");
-        // get a reference to the TextView in this xml so we can change the text
+        isTheme1 = intent.getBooleanExtra("THEME", true);
+        if(isTheme1) {
+            setTheme(R.style.Theme_MyTheme);
+        } else {
+            setTheme(R.style.Theme_MyTheme2);
+        }
         setContentView(R.layout.activity_second);
+        String text = intent.getStringExtra("TEXT");
+        textView = findViewById(R.id.textView);
+        textView.setText(text);
+    }
 
+    public void returnBack(View v){
+        Intent intent = new Intent(this, MainActivity.class);;
+        startActivity(intent);
     }
 }
